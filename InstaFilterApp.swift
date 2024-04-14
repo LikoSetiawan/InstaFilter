4b9839582efb686b805c1b60be7060179ba40cdb
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-04-14 20:34:48 +0800

    Initial commit

---
+++ InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Liko Setiawan on 14/04/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
